052da7a9fb4f464674eee4ad9a607bf5cc82839b
Pauline Perrin <paulineperrin63@hotmail.fr>
2019-05-06 09:40:41 +0200

    design client 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,40 @@ import { AppComponent } from './app.component';
 import { AcceuilComponent } from './components/acceuil/acceuil.component';
 import { ClientComponent } from './components/client/client.component';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NewclientComponent } from './components/newclient/newclient.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AcceuilComponent,
-    ClientComponent
+    ClientComponent,
+    NewclientComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
 
       {
         path: '',
         component: AcceuilComponent
+      },
+      {
+        path: 'client',
+        component: ClientComponent
+      },
+      {
+        path: 'ajouterclient',
+        component: NewclientComponent
+      },
+      {
+        path: 'ajouterclient/:client_id',
+        component: NewclientComponent
       }
     ])
   ],
--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Client } from 'src/app/model/Client';
+import { ClientServiceService } from '../../services/client-service.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-client',
@@ -6,10 +10,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client.component.css']
 })
 export class ClientComponent implements OnInit {
+  clients: Client[];
+  client: Client;
 
-  constructor() { }
 
-  ngOnInit() {
+
+  constructor(private router: Router, private route: ActivatedRoute,
+              private clientService: ClientServiceService, private location: Location) {
+    }
+
+  ngOnInit(): void {
+
+
   }
 
+
+goBackButtonPressed(): void {
+  this.location.back();
 }
+
+ajouterclient(): void {
+  this.router.navigateByUrl('/ajouterclient');
+}
+//ajouterclient(): void {
+ // this.router.navigateByUrl('/ajouterClient/' + this.client.id);
+//}
+
+public deleteButtonPressed(): void {
+  this.clientService.deleteClient(this.client).subscribe(
+    (response) => {
+      this.router.navigateByUrl('/Clients');
+    }
+  );
+}
+
+public getClientById(id: number): void {
+  this.clientService.getClientById(id).subscribe((response) => {
+    this.client = response;
+    console.log(this.client);
+  });
+}
+
+
+
+}
+
+
